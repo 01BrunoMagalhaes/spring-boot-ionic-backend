ebf75157f700f29d6761ba12cf628e8ee4c8183f
Bruno Magalhães <bruno.magalhaes.parreira@gmail.com>
2019-03-25 16:00:32 -0300

    Inserindo Pedido

--- src/main/java/com/brunomagalhaes/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/brunomagalhaes/cursomc/domain/PagamentoComCartao.java
@@ -3,8 +3,10 @@ package com.brunomagalhaes.cursomc.domain;
 import javax.persistence.Entity;
 
 import com.brunomagalhaes.cursomc.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
